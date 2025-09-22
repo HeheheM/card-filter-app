259c8447186b6476bd43abc25287a907e5391bcd
HeheheM <kefirowaty123@gmail.com>
2025-09-22 05:23:10 +0200

    Updates

---
+++ precache-manifest.a6fcb1f856180d4a8c450cb5a47a7e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bc518e3c46509ac592eee8f609393d",
+    "url": "/card-filter-app/index.html"
+  },
+  {
+    "revision": "14e6464f711562b149f8",
+    "url": "/card-filter-app/static/css/main.e12650e2.chunk.css"
+  },
+  {
+    "revision": "45caf72e15549226d147",
+    "url": "/card-filter-app/static/js/2.b7257ad6.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/card-filter-app/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "14e6464f711562b149f8",
+    "url": "/card-filter-app/static/js/main.56cc0f85.chunk.js"
+  },
+  {
+    "revision": "7c0f94e5c246ef103685",
+    "url": "/card-filter-app/static/js/runtime~main.6a35b065.js"
+  }
+]);
